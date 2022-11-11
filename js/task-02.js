7cc5cd847cd025acfd7dd9b8de2637ae0b7944f0
Rostyslav-Baryshok <85187900+Rostyslav-Baryshok@users.noreply.github.com>
2022-11-11 11:56:30 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,23 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const getItems = (elements) => {
+  return elements.map((element) => {
+    const itemEl = document.createElement("li");
+    itemEl.classList.add("item");
+    itemEl.textContent = element;
+
+    console.log(itemEl);
+    return itemEl;
+  });
+};
+
+const items = getItems(ingredients);
+const ingredientsList = document.querySelector("#ingredients");
+ingredientsList.append(...items);
