5bad84f0a73da17c7a5c1bd38cd60b9b8956a345
Rostyslav-Baryshok <85187900+Rostyslav-Baryshok@users.noreply.github.com>
2022-11-18 17:28:42 +0200

    update, fix

--- js/task-04.js
+++ js/task-04.js
@@ -4,3 +4,20 @@ incrementBtn: document.querySelector('[data-action="increment"]');
 valueEl: document.querySelector("#value");
 
 let counterValue = 0;
+
+counter.decrementBtn.addEventListener("click", decrement);
+counter.incrementBtb.addEventListener("click", increment);
+
+function decrement() {
+  counterValue -= 1;
+  changeValueEl(counterValue);
+}
+
+function increment() {
+  counterValue += 1;
+  changeValueEl(counterValue);
+}
+
+function changeValueEl(value) {
+  counter.valueEl.textContent(value);
+}
