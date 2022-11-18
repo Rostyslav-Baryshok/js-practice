8ba3abc1e277ad7eebd5832eea42df61090bd1fc
Rostyslav-Baryshok <85187900+Rostyslav-Baryshok@users.noreply.github.com>
2022-11-18 17:19:09 +0200

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,6 @@
+const counter = {};
+decrementBtn: document.querySelector('[data-action="decrement"]');
+incrementBtn: document.querySelector('[data-action="increment"]');
+valueEl: document.querySelector("#value");
+
+let counterValue = 0;
